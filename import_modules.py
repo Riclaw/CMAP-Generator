2e14dc77aa6ef70d62fb89b05c37b0c20f132028
Luca Matarazzo <luca.silvestro.matarazzo@outlook.it>
2023-05-18 16:32:21 +0200

    Created new  files to store modules and graph.

---
+++ import_modules.py
@@ -0,0 +1,17 @@
+# Modules to be imported
+
+import   pydot, os
+import   subprocess, sys
+import   pickle, graphviz
+import   pandas as pd
+import   numpy as np
+from     pyopenie import OpenIE5
+from     unidecode import unidecode
+import   networkx as nx
+import   pylab as plt
+from     networkx.drawing.nx_agraph import graphviz_layout
+
+pd.options.mode.chained_assignment = None 
+
+
+print("All imported")
