5c0affec643226980167d5552d9bca32df65d155
domitillaizzo98 <126577046+domizzo98@users.noreply.github.com>
2023-05-19 00:39:21 +0200

    relation extraction

--- import_modules.py
+++ import_modules.py
@@ -1,15 +1,16 @@
 # Modules to be imported
 
+import   requests
 import   pydot, os
 import   subprocess, sys
 import   pickle, graphviz
+import   json, re, nltk
 import   pandas as pd
 import   numpy as np
 from     pyopenie import OpenIE5
 from     unidecode import unidecode
-import   networkx as nx
-import   pylab as plt
-from     networkx.drawing.nx_agraph import graphviz_layout
+from     nltk.tokenize import sent_tokenize
+
 
 pd.options.mode.chained_assignment = None 
 
